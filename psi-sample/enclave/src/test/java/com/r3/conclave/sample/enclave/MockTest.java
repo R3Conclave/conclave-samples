bf9e38234d72a38a75ff5ae8b6702a04ec641d4e
snehadamle <sneha.damle@r3.com>
2021-06-16 10:08:58 +0530

    Adding enclave unit test

---
+++ psi-sample/enclave/src/test/java/com/r3/conclave/sample/enclave/MockTest.java
@@ -0,0 +1,50 @@
+package com.r3.conclave.sample.enclave;
+
+import com.r3.conclave.host.EnclaveLoadException;
+import com.r3.conclave.sample.common.AdDetails;
+import com.r3.conclave.sample.common.UserDetails;
+import com.r3.conclave.testing.MockHost;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * This test class calculates the ad conversion rate given the inputs.
+ */
+public class MockTest {
+
+    @Test
+    void calculateAdConversionRate() throws EnclaveLoadException {
+        MockHost<PSIEnclave> mockHost = MockHost.loadMock(PSIEnclave.class);
+        mockHost.start(null, null);
+        PSIEnclave psiEnclave = mockHost.getEnclave();
+
+        List<UserDetails> userDetailsList = new ArrayList();
+        UserDetails userDetails1 = new UserDetails("123");
+        UserDetails userDetails2 = new UserDetails("456");
+        userDetailsList.add(userDetails1);
+        userDetailsList.add(userDetails2);
+
+        List<AdDetails> adDetailsList = new ArrayList();
+        AdDetails adDetails1 = new AdDetails("123");
+        AdDetails adDetails2 = new AdDetails("456");
+        AdDetails adDetails3 = new AdDetails("789");
+        AdDetails adDetails4 = new AdDetails("111");
+        AdDetails adDetails5 = new AdDetails("222");
+
+        adDetailsList.add(adDetails1);
+        adDetailsList.add(adDetails2);
+        adDetailsList.add(adDetails3);
+        adDetailsList.add(adDetails4);
+        adDetailsList.add(adDetails5);
+
+        Double adConversionRate = psiEnclave.getAdConversionRate(userDetailsList, adDetailsList);
+
+        assertNotNull(adConversionRate);
+        assertEquals(40.0, adConversionRate);
+    }
+}
