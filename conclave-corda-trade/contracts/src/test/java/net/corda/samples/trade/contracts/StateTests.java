5e1f7c1ea69e1379042dc5db92e2a9e6717bf8da
Ashutosh Meher <ashutosh.meher@r3.com>
2021-08-31 16:27:48 +0530

    Removing unwanted files

--- conclave-corda-trade/contracts/src/test/java/net/corda/samples/trade/contracts/StateTests.java
+++ conclave-corda-trade/contracts/src/test/java/net/corda/samples/trade/contracts/StateTests.java
@@ -1,18 +1,9 @@
-//package net.corda.samples.trading.contracts;
-//
-//import net.corda.testing.node.MockServices;
-//import org.junit.Test;
-//
-//import java.util.UUID;
-//
-//public class StateTests {
-//    private final MockServices ledgerServices = new MockServices();
-//
-//    @Test
-//    public void hasFieldOfCorrectType() throws NoSuchFieldException {
-//        // Does the message field exist?
-//        AuctionState.class.getDeclaredField("auctionId");
-//        // Is the message field of the correct type?
-//        assert(AuctionState.class.getDeclaredField("auctionId").getType().equals(UUID.class));
-//    }
-//}
+package net.corda.samples.trade.contracts;
+
+import net.corda.testing.node.MockServices;
+
+public class StateTests {
+    private final MockServices ledgerServices = new MockServices();
+
+
+}
