49947fcb3a481a2984b71b53a6d1c1a50d21b9d3
sneha.damle <sneha.damle@r3.com>
2021-11-17 15:23:08 +0530

    1. removed kryo 2. added Role enum for client type 3. added serialization-config.json 4. applied some sonar fixes

--- psi-sample/client/src/main/java/com/r3/conclave/sample/client/Client.java
+++ psi-sample/client/src/main/java/com/r3/conclave/sample/client/Client.java
@@ -1,15 +1,18 @@
 package com.r3.conclave.sample.client;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Output;
 import com.r3.conclave.client.EnclaveClient;
 import com.r3.conclave.client.web.WebEnclaveTransport;
 import com.r3.conclave.common.EnclaveConstraint;
 import com.r3.conclave.mail.EnclaveMail;
-import com.r3.conclave.sample.common.*;
+import com.r3.conclave.sample.common.AdDetails;
+import com.r3.conclave.sample.common.InputData;
+import com.r3.conclave.sample.common.Role;
+import com.r3.conclave.sample.common.UserDetails;
 import picocli.CommandLine;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -21,113 +24,109 @@ import java.util.concurrent.Callable;
  * Both the clients send the lists to enclave, which calculates the ad conversion rate and sends it back to the clients
  */
 @CommandLine.Command(name = "psi-client",
-        mixinStandardHelpOptions = true,
-        description = "Simple client that communicates with the PSIEnclave using the web host.")
+		mixinStandardHelpOptions = true,
+		description = "Simple client that communicates with the PSIEnclave using the web host.")
 public class Client implements Callable<Void> {
 
-    @CommandLine.Option(names = {"-r", "--role"}
-            , description = "Role Options: ${MERCHANT/SERVICE_PROVIDER}")
-    Role role = null;
+	@CommandLine.Option(names = {"-r", "--role"}
+			, description = "Role Options: ${MERCHANT/SERVICE_PROVIDER}")
+	private static Role role = null;
 
-    //use picocli to provide command line parameters
-    @CommandLine.Parameters( description = "List of credit card numbers to be sent to enclave")
-        private List<String> creditCardNumbers = new ArrayList<String>();
+	//use picocli to provide command line parameters
+	@CommandLine.Parameters(description = "List of credit card numbers to be sent to enclave")
+	private final List<String> creditCardNumbers = new ArrayList<>();
 
-    @CommandLine.Option(names = {"-u", "--url"},
-                required = true,
-                description = "URL of the web host running the enclave.")
-        private String url;
+	@CommandLine.Option(names = {"-u", "--url"},
+			required = true,
+			description = "URL of the web host running the enclave.")
+	private String url;
 
-    @CommandLine.Option(names = {"-c", "--constraint"},
-                required = true,
-                description = "Enclave constraint which determines the enclave's identity and whether it's acceptable to use.",
-                converter = EnclaveConstraintConverter.class)
-    private EnclaveConstraint constraint;
+	@CommandLine.Option(names = {"-c", "--constraint"},
+			required = true,
+			description = "Enclave constraint which determines the enclave's identity and whether it's acceptable to use.",
+			converter = EnclaveConstraintConverter.class)
+	private EnclaveConstraint constraint;
 
-    /**
-     * Use Kryo to serialize inputs from client to enclave
-     */
-    private Output serializeMessage(InputData listOfCreditCardNumbers){
-        Kryo kryo = new Kryo();
-        Output output = new Output(new ByteArrayOutputStream());
-        kryo.register(InputData.class, new InputDataSerializer());
-        kryo.writeObject(output, listOfCreditCardNumbers);
-        output.close();
-        return output;
-    }
+	@Override
+	public Void call() throws Exception {
+		//a new private key is generated. Enclave Client is created using this private key and constraint.
+		//a corresponding public key will be used by the enclave to encrypt data to be sent to this client
+		EnclaveClient enclaveClient = new EnclaveClient(constraint);
 
-    @Override
-    public Void call() throws Exception {
-        //a new private key is generated. Enclave Client is created using this private key and constraint.
-        //a corresponding public key will be used by the enclave to encrypt data to be sent to this client
-        EnclaveClient enclaveClient = new EnclaveClient(constraint);
+		try (WebEnclaveTransport transport = new WebEnclaveTransport(url);
+		     EnclaveClient client = enclaveClient) {
+			//retrieve the enclaveInstanceInfo object, i.e. the remote attestation object and verify it against the
+			//constraint
+			client.start(transport);
 
-        try (WebEnclaveTransport transport = new WebEnclaveTransport(url);
-             EnclaveClient client = enclaveClient)
-        {
-            //retrieve the enclaveInstanceInfo object, i.e. the remote attestation object and verify it against the
-            //constraint
-            client.start(transport);
+			//collect merchants and service providers credit card numbers list
+			InputData inputData = getInputData();
 
-            //collect merchants and service providers credit card numbers list
-            InputData inputData = getInputData();
+			byte[] requestMailBody = serialize(inputData);
 
-            byte[] requestMailBody = serializeMessage(inputData).getBuffer();
+			//client will send its credit card list to enclave and wait for other client to send their list
+			EnclaveMail responseMail = client.sendMail(requestMailBody);
 
-            //client will send its credit card list to enclave and wait for other client to send their list
-            EnclaveMail responseMail = client.sendMail(requestMailBody);
+			//responseMail is null till enclave doesn't reply back to the client
+			if (responseMail == null) {
+				do {
+					Thread.sleep(2000);
+					//poll for reply to enclave
+					responseMail = enclaveClient.pollMail();
+				} while (responseMail == null);
+			}
+			System.out.println("Ad Conversion Rate is : " + new String(responseMail.getBodyAsBytes()));
+		}
+		return null;
+	}
 
-            //responseMail is null till enclave doesn't reply back to the client
-            if(responseMail == null) {
-                do {
-                    Thread.sleep(2000);
-                    //poll for reply to enclave
-                    responseMail = enclaveClient.pollMail();
-                } while (responseMail==null);
-            }
-            System.out.println("Ad Conversion Rate is : " + new String(responseMail.getBodyAsBytes()));
-        }
-        return null;
-    }
+	private InputData getInputData() {
+		InputData inputData = new InputData();
+		List<UserDetails> userDetailsList;
+		List<AdDetails> adDetailsList;
 
-    private InputData getInputData() {
-        InputData inputData = new InputData();
-        List<UserDetails> userDetailsList = null;
-        List<AdDetails> adDetailsList = null;
+		inputData.setClientType(role);
 
-        inputData.setClientType(role.name());
+		if (role == Role.MERCHANT) {
+			userDetailsList = new ArrayList<>(creditCardNumbers.size());
+			for (String creditCardNumber : creditCardNumbers) {
+				UserDetails userDetails = new UserDetails(creditCardNumber);
+				userDetailsList.add(userDetails);
+			}
+			inputData.setUserDetailsList(userDetailsList);
 
-        if (Role.MERCHANT.name().equals(role.name())) {
-            userDetailsList = new ArrayList(creditCardNumbers.size());
-            for (int i = 0; i < creditCardNumbers.size(); i++) {
-                UserDetails userDetails = new UserDetails(creditCardNumbers.get(i));
-                userDetailsList.add(userDetails);
-            }
-            inputData.setUserDetailsList(userDetailsList);
+		} else if (role == Role.SERVICE_PROVIDER) {
+			adDetailsList = new ArrayList<>(creditCardNumbers.size());
+			for (String creditCardNumber : creditCardNumbers) {
+				AdDetails adDetails = new AdDetails(creditCardNumber);
+				adDetailsList.add(adDetails);
+			}
+			inputData.setAdDetailsList(adDetailsList);
+		}
+		return inputData;
+	}
 
-        }
-        else if (Role.SERVICE_PROVIDER.name().equals(role.name())) {
-            System.out.println("inside SERVICE_PROVIDER");
+	public static byte[] serialize(Object obj) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ObjectOutputStream os;
+		try {
+			os = new ObjectOutputStream(out);
+			os.writeObject(obj);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return out.toByteArray();
+	}
 
-            adDetailsList = new ArrayList<>(creditCardNumbers.size());
-            for (int i = 0; i < creditCardNumbers.size(); i++) {
-                AdDetails adDetails = new AdDetails(creditCardNumbers.get(i));
-                adDetailsList.add(adDetails);
-            }
-            inputData.setAdDetailsList(adDetailsList);
-        }
-        return inputData;
-    }
+	private static class EnclaveConstraintConverter implements CommandLine.ITypeConverter<EnclaveConstraint> {
+		@Override
+		public EnclaveConstraint convert(String value) {
+			return EnclaveConstraint.parse(value);
+		}
+	}
 
-    private static class EnclaveConstraintConverter implements CommandLine.ITypeConverter<EnclaveConstraint> {
-        @Override
-        public EnclaveConstraint convert(String value) {
-            return EnclaveConstraint.parse(value);
-        }
-    }
-
-    public static void main(String... args) {
-        int exitCode = new CommandLine(new Client()).execute(args);
-        System.exit(exitCode);
-    }
+	public static void main(String... args) {
+		int exitCode = new CommandLine(new Client()).execute(args);
+		System.exit(exitCode);
+	}
 }
